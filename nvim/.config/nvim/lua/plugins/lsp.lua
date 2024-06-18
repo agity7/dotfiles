a9143712877355c9127afd5ebcec2532fcc2a6b3
Philippe Bazinet <pbazinet@fabriktor.com>
2024-06-17 21:22:38 -0400

    docs: enhanced readme

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,28 +1,28 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "stylua",
-        "selene",
-        "luacheck",
-        "shellcheck",
-        "shfmt",
-        "css-lsp",
-        "dart-debug-adapter",
-        "dockerfile-language-server",
-        "gitlab-ci-ls",
-        "gofumpt",
-        "goimports",
-        "gopls",
-        "hadolint",
-        "json-lsp",
-        "lua-language-server",
-        "prettier",
-        "shfmt",
-        "sqlfluff",
-        "yaml-language-server",
-      })
-    end,
-  },
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"stylua",
+				"selene",
+				-- "luacheck",
+				"shellcheck",
+				"shfmt",
+				"css-lsp",
+				"dart-debug-adapter",
+				"dockerfile-language-server",
+				"gitlab-ci-ls",
+				"gofumpt",
+				"goimports",
+				"gopls",
+				"hadolint",
+				"json-lsp",
+				"lua-language-server",
+				"prettier",
+				"shfmt",
+				"sqlfluff",
+				"yaml-language-server",
+			})
+		end,
+	},
 }
