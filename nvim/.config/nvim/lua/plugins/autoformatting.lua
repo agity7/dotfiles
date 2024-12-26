9e7bfb908816126529addf93ea95383667f5daff
Philippe Bazinet <pbazinet@fabriktor.com>
2024-12-26 10:28:54 -0500

    fix: add missing go tools in autoformatting.lya

--- nvim/.config/nvim/lua/plugins/autoformatting.lua
+++ nvim/.config/nvim/lua/plugins/autoformatting.lua
@@ -8,6 +8,7 @@ return {
 		local null_ls = require("null-ls")
 		local formatting = null_ls.builtins.formatting -- to setup formatters
 		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
+		local actions = null_ls.builtins.code_actions -- to setup code actions
 
 		-- list of formatters & linters for mason to install
 		require("mason-null-ls").setup({
@@ -16,9 +17,13 @@ return {
 				"prettier", -- ts/js formatter
 				"stylua", -- lua formatter
 				"shfmt",
-				"ruff",
 				"golangci-lint",
 				"goimports-reviser",
+				"gomodifytags",
+				"staticcheck",
+				"gofmt",
+				"goimports",
+				"golines",
 			},
 			-- auto-install configured formatters & linters (with null-ls)
 			automatic_installation = true,
@@ -29,11 +34,13 @@ return {
 			formatting.prettier.with({ filetypes = { "html", "json", "yaml", "markdown" } }),
 			formatting.stylua,
 			formatting.shfmt.with({ args = { "-i", "4" } }),
-			formatting.terraform_fmt,
-			require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
-			require("none-ls.formatting.ruff_format"),
 			diagnostics.golangci_lint.with({ filetypes = { "go" } }),
 			formatting.goimports_reviser.with({ filetypes = { "go" } }),
+			actions.gomodifytags.with({ filetypes = { "go" } }),
+			diagnostics.staticcheck.with({ filetypes = { "go" } }),
+			formatting.gofmt.with({ filetypes = { "go" } }),
+			formatting.goimports.with({ filetypes = { "go" } }),
+			formatting.golines.with({ filetypes = { "go" } }),
 		}
 
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
