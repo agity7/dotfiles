d639f04ac9e40924845388dabd917c377c43e68a
Philippe <pbazinet@fabriktor.com>
2025-07-03 17:52:05 -0400

    fix: enhance codeium behavior

--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -13,13 +13,25 @@ return {
 				map_keys = true,
 				accept_fallback = "<Tab>",
 				idle_delay = 1,
+				on_complete = function()
+					local fname = vim.fn.expand("%:p")
+					if fname ~= "" and vim.fn.filereadable(fname) == 1 then
+						local encoding = vim.fn.system("file -b --mime-encoding " .. vim.fn.shellescape(fname))
+						if not encoding:match("utf%-8") then
+							vim.b.codeium_enabled = false
+							return false
+						end
+					end
+					return true
+				end,
 			},
 		})
-		vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "InsertEnter", "BufReadPost" }, {
-			callback = function()
-				local bt = vim.bo.buftype
-				local ft = vim.bo.filetype
-				local fname = vim.fn.expand("%:p")
+		vim.api.nvim_create_autocmd("BufReadPost", {
+			callback = function(args)
+				local bufnr = args.buf
+				local bt = vim.bo[bufnr].buftype
+				local ft = vim.bo[bufnr].filetype
+				local fname = vim.api.nvim_buf_get_name(bufnr)
 				local disallowed_buftypes = {
 					prompt = true,
 					quickfix = true,
@@ -34,15 +46,20 @@ return {
 					[""] = true,
 				}
 				if disallowed_buftypes[bt] or disallowed_filetypes[ft] or fname:match("%.codeium/") then
-					vim.b.codeium_enabled = false
+					vim.b[bufnr].codeium_enabled = false
 					return
 				end
 				if fname ~= "" and vim.fn.filereadable(fname) == 1 then
 					local encoding = vim.fn.system("file -b --mime-encoding " .. vim.fn.shellescape(fname))
-					if not encoding:match("utf%-8") then
-						vim.b.codeium_enabled = false
-						if vim.api.nvim_get_current_buf() == vim.api.nvim_get_current_win() then
-							vim.notify("[Codeium] Disabled on non-UTF-8 file: " .. fname, vim.log.levels.INFO)
+					if not encoding:match("utf%-8") and not encoding:match("us%-ascii") then
+						vim.b[bufnr].codeium_enabled = false
+						for _, win in ipairs(vim.api.nvim_list_wins()) do
+							if vim.api.nvim_win_get_buf(win) == bufnr then
+								vim.schedule(function()
+									vim.notify("[Codeium] Disabled on non-UTF-8 file: " .. fname, vim.log.levels.INFO)
+								end)
+								break
+							end
 						end
 					end
 				end
