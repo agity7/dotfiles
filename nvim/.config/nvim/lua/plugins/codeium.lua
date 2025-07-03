beeb61a30f3119401ac32d750a9c4e719ca34331
Philippe <pbazinet@fabriktor.com>
2025-07-03 17:03:04 -0400

    fix: edit codeium plugin func to bypass code completion in non utf 8 files

--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -15,5 +15,38 @@ return {
 				idle_delay = 1,
 			},
 		})
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter", "InsertEnter", "BufReadPost" }, {
+			callback = function()
+				local bt = vim.bo.buftype
+				local ft = vim.bo.filetype
+				local fname = vim.fn.expand("%:p")
+				local disallowed_buftypes = {
+					prompt = true,
+					quickfix = true,
+					help = true,
+					terminal = true,
+					nofile = true,
+				}
+				local disallowed_filetypes = {
+					TelescopePrompt = true,
+					TelescopeResults = true,
+					SpectrePanel = true,
+					[""] = true,
+				}
+				if disallowed_buftypes[bt] or disallowed_filetypes[ft] or fname:match("%.codeium/") then
+					vim.b.codeium_enabled = false
+					return
+				end
+				if fname ~= "" and vim.fn.filereadable(fname) == 1 then
+					local encoding = vim.fn.system("file -b --mime-encoding " .. vim.fn.shellescape(fname))
+					if not encoding:match("utf%-8") then
+						vim.b.codeium_enabled = false
+						if vim.api.nvim_get_current_buf() == vim.api.nvim_get_current_win() then
+							vim.notify("[Codeium] Disabled on non-UTF-8 file: " .. fname, vim.log.levels.INFO)
+						end
+					end
+				end
+			end,
+		})
 	end,
 }
